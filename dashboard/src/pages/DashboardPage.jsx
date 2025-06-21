4cbeae3e3d166596d58e67688bb2fd9c1e78a260
Mohan Lal <mohanmarwadi2023@gmail.com>
2025-06-21 22:46:40 +0530

    Dashboard initial setup

---
+++ dashboard/src/pages/DashboardPage.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "../components/dashboard/Dashboard.jsx";
+import TopBar from "../components/common/TopBar.jsx";
+
+const DashboardPage = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+};
+
+export default DashboardPage;
