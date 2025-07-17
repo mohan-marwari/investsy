79fafc4a4c7d61a18c1952a84bddd168feb0a2a9
Mohan Lal <mohanmarwadi2023@gmail.com>
2025-07-18 03:17:42 +0530

    connect database for holdings and positions routes.

--- dashboard/src/components/dashboard/Positions.jsx
+++ dashboard/src/components/dashboard/Positions.jsx
@@ -1,9 +1,19 @@
-import { positions } from "../../data/dummyHoldings.js";
+// import { positions } from "../../data/dummyHoldings.js";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Positions = () => {
+  const [allPositions, setAllPositions] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/api/positions").then((res) => {
+      // console.log(res.data);
+      setAllPositions(res.data);
+    });
+  }, []);
   return (
     <>
-      <h3 className="title">Positions ({positions.length})</h3>
+      <h3 className="title">Positions ({allPositions.length})</h3>
 
       <div className="order-table">
         <table>
@@ -17,7 +27,7 @@ const Positions = () => {
             <th>Chg.</th>
           </tr>
 
-          {positions.map((stock, index) => {
+          {allPositions.map((stock, index) => {
             const curValue = stock.price * stock.qty;
             const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
